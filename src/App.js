aefd59f75ee2989f3986569fe4e15ace182d1d04
Your Name <you@example.com>
2024-11-15 15:30:29 +0530

    add live link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Transpoter from './components/pages/Transpoter'
 import Testimonials from './components/pages/Testimonials'
 import Footer from './components/pages/Footer'
 
+// Read README.md for LIVE DEPLOYED LINK   (https://aaj-enterprises.vercel.app/)
+// run "npm install" before running the project
 const App = () => {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css'; 
 import App from './App';
 
-// Read README.md for LIVE DEPLOYED LINK
+// Read README.md for LIVE DEPLOYED LINK   (https://aaj-enterprises.vercel.app/)
+// run "npm install" before running the project
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
